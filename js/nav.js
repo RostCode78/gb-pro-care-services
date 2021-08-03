c32bef5bf1e5244fdd4cc4cafb2bcc12ca883ffe
Jhonattan Tunales <zeroxcode78@gmail.com>
2021-08-03 16:34:31 -0500

    Feat: Sidebar

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,5 @@
 const menuBtn = document.querySelector('.btn-nav');
+const sidebar = document.querySelector('.sidebar');
 let menuOpen = false;
 
 menuBtn.addEventListener(
@@ -8,11 +9,13 @@ menuBtn.addEventListener(
         if( !menuOpen ) {
 
             menuBtn.classList.add('open');
+            sidebar.classList.add('open');
             menuOpen = true;
 
         } else {
 
             menuBtn.classList.remove('open');
+            sidebar.classList.remove('open');
             menuOpen = false;
             
         }
