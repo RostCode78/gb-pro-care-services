a54ca74b4ac9927920e48ae34ac1c4f6798c099d
Jhonattan Tunales <zeroxcode78@gmail.com>
2021-08-04 18:48:20 -0500

    Fix: Logo

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,6 @@
 const menuBtn = document.querySelector('.btn-nav');
 const sidebar = document.querySelector('.sidebar');
+const text = document.querySelector('.txt-logo-nav');
 let menuOpen = false;
 
 menuBtn.addEventListener(
@@ -10,12 +11,14 @@ menuBtn.addEventListener(
 
             menuBtn.classList.add('open');
             sidebar.classList.add('open');
+            text.classList.add('open');
             menuOpen = true;
 
         } else {
 
             menuBtn.classList.remove('open');
             sidebar.classList.remove('open');
+            text.classList.remove('open');
             menuOpen = false;
             
         }
