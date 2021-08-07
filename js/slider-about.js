1c54a72aa67bf4e77bb13f57d271720b6aef2f4e
Jhonattan Tunales <zeroxcode78@gmail.com>
2021-08-07 05:43:22 -0500

    Feat: Slider About

--- js/slider-about.js
+++ js/slider-about.js
@@ -1,37 +1,102 @@
-const sliderAbout = $('#slider-about');
-const itemsAbout = $$('.item-about');
-let aboutStyle = window.getComputedStyle(sliderAbout).getPropertyValue('width');
-let nextAboutTarget;
-let aboutPosition = 0;
-let widthAbout = aboutStyle.slice(0, -2);
-let aboutTarget = 0;
-sliderAbout.scrollLeft = 0;
+let psa = {
 
-let directionAbout = true;
-let countAbout = 1;
+    // card: $$('.card-opacity'),
+    slider: $('.slider-about'),
+    sliderCount: $$('.slider-active'),
+    item: 0,
+    direction: true,
+    count: 1
 
-let aboutSliderInterval = () => {
+};
 
-    if( countAbout == 1 ) {
-        directionAbout = true;
-    } else if ( countAbout >= itemsAbout.length ) {
-        directionAbout = false;
+let msa = {
+
+    start: () => {
+
+        psa.card.forEach( e => {
+
+            e.addEventListener(
+                'click',
+                msa.activarSlider
+            );
+
+        });
+
+    },
+
+    activarSlider: (e) => {
+    
+        if( e.target == psa.card[0] ) {
+
+            psa.card[0].style.backgroundColor = "#303030d5";
+            psa.card[1].style.backgroundColor = "#30303000";
+            psa.card[2].style.backgroundColor = "#30303000";
+
+        } else if( e.target == psa.card[1] ) {
+
+            psa.card[0].style.backgroundColor = "#30303000";
+            psa.card[1].style.backgroundColor = "#303030d5";
+            psa.card[2].style.backgroundColor = "#30303000";
+
+
+        } else {
+
+            psa.card[0].style.backgroundColor = "#30303000";
+            psa.card[1].style.backgroundColor = "#30303000";
+            psa.card[2].style.backgroundColor = "#303030d5";
+
+        }
+
+        psa.item = e.target.getAttribute("item") - 1;
+        psa.count = e.target.getAttribute("item");
+        psa.slider.style.left = ` ${ psa.item * -100 }% `;
+        clearInterval(psa.counter);
+        psa.counter = setInterval(msa.autoSlider, 5000);
+
+    },
+
+    autoSlider: (e) => {
+
+        if( psa.count == 1 ) {
+            psa.direction = true;
+        } else if ( psa.count >= psa.sliderCount.length ) {
+            psa.direction = false;
+        }
+
+        if ( psa.direction ) {
+            psa.count++;
+        } else {
+            psa.count--;
+        }
+
+        psa.slider.style.left = ` ${ (psa.count - 1) * -100 }% `;
+        // console.log(psa.count);
+        // console.log(psa.slider.style.right);
+    
+        // if( psa.count === 1 ) {
+
+        //     psa.card[0].style.backgroundColor = "#303030d5";
+        //     psa.card[1].style.backgroundColor = "#30303000";
+        //     psa.card[2].style.backgroundColor = "#30303000";
+
+        // } else if( psa.count === 2 ) {
+
+        //     psa.card[0].style.backgroundColor = "#30303000";
+        //     psa.card[1].style.backgroundColor = "#303030d5";
+        //     psa.card[2].style.backgroundColor = "#30303000";
+
+
+        // } else {
+
+        //     psa.card[0].style.backgroundColor = "#30303000";
+        //     psa.card[1].style.backgroundColor = "#30303000";
+        //     psa.card[2].style.backgroundColor = "#303030d5";
+
+        // }
+
     }
 
-    if ( directionAbout ) {
-        countAbout++;
-        aboutPosition++;
-        aboutTarget = aboutPosition;
-        sliderAbout.scrollLeft = widthAbout * aboutTarget;
-        nextAboutTarget = widthAbout * aboutTarget;
-    } else {
-        countAbout--;
-        aboutPosition--;
-        aboutTarget = aboutPosition;
-        sliderAbout.scrollLeft = widthAbout * aboutTarget;
-        nextAboutTarget = widthAbout * aboutTarget;
-    }
+};
 
-}
-
-let counterAbout = setInterval(aboutSliderInterval, 5500);
+// msa.start();
+const counterFunction = setInterval(msa.autoSlider, 4500);
