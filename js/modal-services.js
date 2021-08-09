1972548715fbb2f3b587b72cb0c7e9ccc168ce6a
Jhonattan Tunales <zeroxcode78@gmail.com>
2021-08-08 23:11:00 -0500

    Feat: Modal

--- js/modal-services.js
+++ js/modal-services.js
@@ -1,3 +1,111 @@
+const modalServices01 = `
+<div class="modal-services-content">
+    <div class="close-modal-services">X</div>
+    <div class="image-box-modal-services">
+        <div class="img-modal-services">
+            <img src="./img/services/01.jpeg">
+        </div>
+    </div>
+    <div class="text-box-modal-services">
+        <h2>Shrub Planting</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit dolorum voluptatem minus inventore sequi amet non est nemo facilis, saepe fugit ex. Incidunt vel quod debitis hic explicabo adipisci minima reprehenderit odio sint facilis,
+            repellendus dicta veniam ducimus dolorem perspiciatis doloribus rerum, cupiditate laboriosam laborum, sit saepe! Quae at ab quia possimus voluptas iste voluptatibus. Aperiam atque commodi necessitatibus ducimus explicabo quis quae?
+            Impedit voluptas placeat explicabo officia, expedita unde facilis! Dignissimos similique consectetur mollitia perferendis repellendus a officiis exercitationem! Provident aperiam sed enim accusamus facilis, expedita quasi aspernatur
+            eveniet. Excepturi hic ipsa dicta harum. Veniam exercitationem inventore deleniti.</p>
+    </div>
+</div>
+`;
+
+const modalServices02 = `
+<div class="modal-services-content">
+    <div class="close-modal-services">X</div>
+    <div class="image-box-modal-services">
+        <div class="img-modal-services">
+            <img src="./img/services/02.jpeg">
+        </div>
+    </div>
+    <div class="text-box-modal-services">
+        <h2>Lawn Mowing</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit dolorum voluptatem minus inventore sequi amet non est nemo facilis, saepe fugit ex. Incidunt vel quod debitis hic explicabo adipisci minima reprehenderit odio sint facilis,
+            repellendus dicta veniam ducimus dolorem perspiciatis doloribus rerum, cupiditate laboriosam laborum, sit saepe! Quae at ab quia possimus voluptas iste voluptatibus. Aperiam atque commodi necessitatibus ducimus explicabo quis quae?
+            Impedit voluptas placeat explicabo officia, expedita unde facilis! Dignissimos similique consectetur mollitia perferendis repellendus a officiis exercitationem! Provident aperiam sed enim accusamus facilis, expedita quasi aspernatur
+            eveniet. Excepturi hic ipsa dicta harum. Veniam exercitationem inventore deleniti.</p>
+    </div>
+</div>
+`;
+
+const modalServices03 = `
+<div class="modal-services-content">
+    <div class="close-modal-services">X</div>
+    <div class="image-box-modal-services">
+        <div class="img-modal-services">
+            <img src="./img/services/03.jpeg">
+        </div>
+    </div>
+    <div class="text-box-modal-services">
+        <h2>Garden Care</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit dolorum voluptatem minus inventore sequi amet non est nemo facilis, saepe fugit ex. Incidunt vel quod debitis hic explicabo adipisci minima reprehenderit odio sint facilis,
+            repellendus dicta veniam ducimus dolorem perspiciatis doloribus rerum, cupiditate laboriosam laborum, sit saepe! Quae at ab quia possimus voluptas iste voluptatibus. Aperiam atque commodi necessitatibus ducimus explicabo quis quae?
+            Impedit voluptas placeat explicabo officia, expedita unde facilis! Dignissimos similique consectetur mollitia perferendis repellendus a officiis exercitationem! Provident aperiam sed enim accusamus facilis, expedita quasi aspernatur
+            eveniet. Excepturi hic ipsa dicta harum. Veniam exercitationem inventore deleniti.</p>
+    </div>
+</div>
+`;
+
+const modalServices04 = `
+<div class="modal-services-content">
+    <div class="close-modal-services">X</div>
+    <div class="image-box-modal-services">
+        <div class="img-modal-services">
+            <img src="./img/services/04.jpeg">
+        </div>
+    </div>
+    <div class="text-box-modal-services">
+        <h2>Clean Work</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit dolorum voluptatem minus inventore sequi amet non est nemo facilis, saepe fugit ex. Incidunt vel quod debitis hic explicabo adipisci minima reprehenderit odio sint facilis,
+            repellendus dicta veniam ducimus dolorem perspiciatis doloribus rerum, cupiditate laboriosam laborum, sit saepe! Quae at ab quia possimus voluptas iste voluptatibus. Aperiam atque commodi necessitatibus ducimus explicabo quis quae?
+            Impedit voluptas placeat explicabo officia, expedita unde facilis! Dignissimos similique consectetur mollitia perferendis repellendus a officiis exercitationem! Provident aperiam sed enim accusamus facilis, expedita quasi aspernatur
+            eveniet. Excepturi hic ipsa dicta harum. Veniam exercitationem inventore deleniti.</p>
+    </div>
+</div>
+`;
+
+const modalServices05 = `
+<div class="modal-services-content">
+    <div class="close-modal-services">X</div>
+    <div class="image-box-modal-services">
+        <div class="img-modal-services">
+            <img src="./img/services/05.jpeg">
+        </div>
+    </div>
+    <div class="text-box-modal-services">
+        <h2>Garden Care</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit dolorum voluptatem minus inventore sequi amet non est nemo facilis, saepe fugit ex. Incidunt vel quod debitis hic explicabo adipisci minima reprehenderit odio sint facilis,
+            repellendus dicta veniam ducimus dolorem perspiciatis doloribus rerum, cupiditate laboriosam laborum, sit saepe! Quae at ab quia possimus voluptas iste voluptatibus. Aperiam atque commodi necessitatibus ducimus explicabo quis quae?
+            Impedit voluptas placeat explicabo officia, expedita unde facilis! Dignissimos similique consectetur mollitia perferendis repellendus a officiis exercitationem! Provident aperiam sed enim accusamus facilis, expedita quasi aspernatur
+            eveniet. Excepturi hic ipsa dicta harum. Veniam exercitationem inventore deleniti.</p>
+    </div>
+</div>
+`;
+
+const modalServices06 = `
+<div class="modal-services-content">
+    <div class="close-modal-services">X</div>
+    <div class="image-box-modal-services">
+        <div class="img-modal-services">
+            <img src="./img/services/06.jpeg">
+        </div>
+    </div>
+    <div class="text-box-modal-services">
+        <h2>Lawn Mowing</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit dolorum voluptatem minus inventore sequi amet non est nemo facilis, saepe fugit ex. Incidunt vel quod debitis hic explicabo adipisci minima reprehenderit odio sint facilis,
+            repellendus dicta veniam ducimus dolorem perspiciatis doloribus rerum, cupiditate laboriosam laborum, sit saepe! Quae at ab quia possimus voluptas iste voluptatibus. Aperiam atque commodi necessitatibus ducimus explicabo quis quae?
+            Impedit voluptas placeat explicabo officia, expedita unde facilis! Dignissimos similique consectetur mollitia perferendis repellendus a officiis exercitationem! Provident aperiam sed enim accusamus facilis, expedita quasi aspernatur
+            eveniet. Excepturi hic ipsa dicta harum. Veniam exercitationem inventore deleniti.</p>
+    </div>
+</div>
+`;
+
 const pMS = {
     openModal: $$('.box-service'),
     modal: $('.modal-services-container'),
@@ -32,13 +140,19 @@ const mMS = {
         pMS.modal.style.display = 'flex';
 
         if ( e.target == pMS.openModal[0] ) {
-            pMS.openModal[0].style.backgroundColor = "#45a3a2"
+            pMS.modal.innerHTML = modalServices01;
+        } else if ( e.target == pMS.openModal[1] ) {
+            pMS.modal.innerHTML = modalServices02;
+        } else if ( e.target == pMS.openModal[2] ) {
+            pMS.modal.innerHTML = modalServices03;
+        } else if ( e.target == pMS.openModal[3] ) {
+            pMS.modal.innerHTML = modalServices04;
+        } else if ( e.target == pMS.openModal[4] ) {
+            pMS.modal.innerHTML = modalServices05;
+        } else if ( e.target == pMS.openModal[5] ) {
+            pMS.modal.innerHTML = modalServices06;
         } else {
-            pMS.openModal[1].style.backgroundColor = "#f24d32"
-            pMS.openModal[2].style.backgroundColor = "#f24d32"
-            pMS.openModal[3].style.backgroundColor = "#f24d32"
-            pMS.openModal[4].style.backgroundColor = "#f24d32"
-            pMS.openModal[5].style.backgroundColor = "#f24d32"
+            pMS.modal.innerHTML = modalServices01;
         }
 
     },
